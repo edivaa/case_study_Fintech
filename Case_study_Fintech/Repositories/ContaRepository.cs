98e216bb2287d6f879ba886c4609e1d117e243d1
♫ ♫ Edivã ♫ ♫ <edivaa@gmail.com>
2021-06-10 18:19:58 -0300

    feat: account repository creation

---
+++ Case_study_Fintech/Repositories/ContaRepository.cs
@@ -0,0 +1,23 @@
+﻿using Case_study_Fintech.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Case_study_Fintech.Repositories
+{
+    public class ContaRepository
+    {
+        
+        public List<Conta> GetContas()
+        {
+            return null;
+        }
+
+        public Conta GetConta(int idConta)
+        {
+            return null;
+        }
+
+    }
+}
