ba82640bdfe12fab3049a24da2ab926cb596a8a9
♫ ♫ Edivã ♫ ♫ <edivaa@gmail.com>
2021-06-10 18:48:44 -0300

    feat: account service creation

---
+++ Case_study_Fintech/Services/ContaService.cs
@@ -0,0 +1,30 @@
+﻿using Case_study_Fintech.Models;
+using Case_study_Fintech.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Case_study_Fintech.Services
+{
+    public class ContaService
+    {
+        readonly ContaRepository contaRepository;
+         
+        public ContaService()
+        {
+            contaRepository = new ContaRepository();
+        }
+
+
+        public List<Conta> GetContas()
+        {
+            return contaRepository.GetContas();
+        }
+
+        public Conta GetConta(int id)
+        {
+            return contaRepository.GetConta(id);
+        }
+    }
+}
