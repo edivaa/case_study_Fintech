e3617298c74d3344f42c120e956287872d450ab8
♫ ♫ Edivã ♫ ♫ <edivaa@gmail.com>
2021-06-10 17:10:23 -0300

    feat: test project start and setup

---
+++ TestCase_study_Fintech/Conta/ContaTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestCase_study_Fintech
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CriarConta()
+        {
+            Assert.Pass();
+        }
+    }
+}
