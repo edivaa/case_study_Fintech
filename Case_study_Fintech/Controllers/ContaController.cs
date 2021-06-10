82823c04601275d54edd36e7e32a1c979f0886b5
♫ ♫ Edivã ♫ ♫ <edivaa@gmail.com>
2021-06-10 15:56:48 -0300

    feat: account control creation

---
+++ Case_study_Fintech/Controllers/ContaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Case_study_Fintech.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContaController : ControllerBase
+    {
+      
+        private readonly ILogger<ContaController> _logger;
+
+        public ContaController(ILogger<ContaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("ObterSaldoConta")]
+        public IActionResult Post(object conta)
+        {
+
+            return Ok(new {saldo = 35256663});
+        }
+
+
+    }
+}
