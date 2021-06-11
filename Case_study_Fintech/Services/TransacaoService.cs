a80f63fac729aeec186336b8f5885859aad5b8e9
EdivaGt Linux <edivaa@gmail.com>
2021-06-11 13:01:56 -0300

    feat: criacao de service para transacao

---
+++ Case_study_Fintech/Services/TransacaoService.cs
@@ -0,0 +1,30 @@
+﻿using Case_study_Fintech.Models;
+using Case_study_Fintech.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Case_study_Fintech.Services
+{
+    public class TransacaoService
+    {
+        readonly ContaRepository contaRepository;
+         private Random random = new Random();
+        public TransacaoService()
+        {
+            contaRepository = new ContaRepository();
+        }
+        public List<Conta> GetContas()
+        {
+            return contaRepository.GetContas();
+        }
+    
+        public Decimal SacarPix(int numConta) {
+
+               //var contaExiste = contaRepository.GetConta(numConta);
+
+               throw new NotImplementedException();
+        }
+    }
+}
