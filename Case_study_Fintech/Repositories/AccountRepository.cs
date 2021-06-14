6b3dac1024af770410a768c6e1ba3e0901d1ba54
♫ ♫ Edivã ♫ ♫ <edivaa@gmail.com>
2021-06-14 11:57:17 -0300

    fix: adjust account repository name -  two

--- Case_study_Fintech/Repositories/AccountRepository.cs
+++ Case_study_Fintech/Repositories/AccountRepository.cs
@@ -8,11 +8,11 @@ namespace Case_study_Fintech.Repositories
 {
     public class AccountRepository
     {
-        private List<Account> contas;
+        private List<Account> accounts;
 
         public AccountRepository()
         {
-          contas  = new List<Account>() {
+          accounts  = new List<Account>() {
                 new Account() { Name = "Antonio", LastName = "Santos", Email = "antonio@gmail.com", AccountNumber = 12556, Balance =1000000 },
                 new Account() { Name = "Aiton", LastName = "Rosa", Email = "aiton@gmail.com", AccountNumber = 32897, Balance =4000000 },
                 new Account() { Name = "Tamara", LastName = "Pitanga", Email = "tamara@gmail.com", AccountNumber = 45896, Balance =9000000 },
@@ -22,16 +22,16 @@ namespace Case_study_Fintech.Repositories
 
         public List<Account> GetAccounts()
         {
-            return contas;
+            return accounts;
         }
 
         public Account GetAccount(int? numConta)
         {
-            return contas.FirstOrDefault(c=>c.AccountNumber == numConta);
+            return accounts.FirstOrDefault(c=>c.AccountNumber == numConta);
         }
 
         public Account AddAccount(Account account) {
-            contas.Add(account);
+            accounts.Add(account);
 
             return GetAccount(account.AccountNumber);
         }
