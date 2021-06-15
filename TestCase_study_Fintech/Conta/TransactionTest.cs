0b4faa6f0a597fb4c5a2e0c626ee552d7db8c38b
♫ ♫ Edivã ♫ ♫ <edivaa@gmail.com>
2021-06-15 18:25:19 -0300

    test: testing minimum amount allowed for withdrawal

--- TestCase_study_Fintech/Conta/TransactionTest.cs
+++ TestCase_study_Fintech/Conta/TransactionTest.cs
@@ -8,8 +8,8 @@ namespace TestCase_study_Fintech
     [Category("NUnit")]
     public class TransactionTest
     {
-        private TransactionService transactionService;  
-       
+        private TransactionService transactionService;
+
 
         [SetUp]
         public void Setup()
@@ -18,33 +18,33 @@ namespace TestCase_study_Fintech
         }
 
 
-        
+
         [Test]
         public void ShouldWithdrawWithBalance()
         {
             //arrange
-            var accountNumber = 12556; 
+            var accountNumber = 12556;
             var value = 2001;
 
             //act
             var withdrawalValue = transactionService.WithdrawMoneyByPix(accountNumber, value);
 
             //Assert 
-            Assert.IsTrue( withdrawalValue > 0);
+            Assert.IsTrue(withdrawalValue > 0);
         }
 
         [Test]
         public void ShouldReturnErrorWithoutBalance()
         {
-             //arrange
-             var accountNumber = 58996;
-             var value = 2001;
+            //arrange
+            var accountNumber = 58996;
+            var value = 2001;
 
-           
-             //assert 
-               Assert.That(() => transactionService.WithdrawMoneyByPix(accountNumber, value),
-                Throws.TypeOf<Exception>()
-                .With.Matches<Exception>(mess => mess.Message == "Conta sem saldo"));
+
+            //assert 
+            Assert.That(() => transactionService.WithdrawMoneyByPix(accountNumber, value),
+             Throws.TypeOf<Exception>()
+             .With.Matches<Exception>(mess => mess.Message == "Conta sem saldo"));
 
         }
 
@@ -54,11 +54,27 @@ namespace TestCase_study_Fintech
             //arrange Balance =1000000
             var accountNumber = 12556;
             var value = 1100000;
-            
+
             //Act //assert 
             Assert.That(() => transactionService.WithdrawMoneyByPix(accountNumber, value),
                 Throws.TypeOf<Exception>()
-                .With.Matches<Exception>(mess => mess.Message =="valor acima do saldo"));
+                .With.Matches<Exception>(mess => mess.Message == "valor acima do saldo"));
+
+        }
+
+        [Test]
+        public void ShouldRetornErrorMinimumAmountAllowedForWithdrawal()
+        {
+
+            //arrange
+            var accountNumber = 58996;
+            var value = 1999;
+
+
+            //assert 
+            Assert.That(() => transactionService.WithdrawMoneyByPix(accountNumber, value),
+             Throws.TypeOf<Exception>()
+             .With.Matches<Exception>(mess => mess.Message == "Valor minimo igual ou acima de 2000"));
 
         }
 
