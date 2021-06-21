ed3be09761022216e282f1141d783eb83b8666de
♫ ♫ Edivã ♫ ♫ <edivaa@gmail.com>
2021-06-21 16:49:00 -0300

    feat: Validation if user exists

--- Case_study_Fintech/Services/UserService.cs
+++ Case_study_Fintech/Services/UserService.cs
@@ -8,24 +8,35 @@ namespace Case_study_Fintech.Services
 {
     public class UserService
     {
-      
+
 
         public User Authentication(User user)
         {
-           
-            return ApiAuthentication(user.UserName, user.Password);
+            try
+            {
+                return ApiAuthentication(user.UserName, user.Password);
+
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Usuário não existe");
+            }
         }
 
 
 
         private User ApiAuthentication(string userName, string password)
         {
-           List<User> loginList = new List<User> { 
+            List<User> loginList = new List<User> {
                new User { UserName = "paulo", Password = "25636" }
            };
 
+            var user = loginList.FirstOrDefault(user => user.UserName.Equals(userName) && user.Password.Equals(password));
 
-            return loginList.FirstOrDefault(user => user.UserName.Equals(userName) && user.Password.Equals(password));
+            if (user == null) throw new Exception("Usuário não existe");
+
+            return user ;
         }
     }
 }
